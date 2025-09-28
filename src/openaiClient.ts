a65f6b644d5868abc9e21cc70c59d0cfb8a9fb1a
M R M Qaaid <qaaidrizan113@gmail.com>
2025-09-28 10:42:41 +0530

    Api changes

--- src/openaiClient.ts
+++ src/openaiClient.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  apiKey: import.meta.env.VITE_GEMINI_API_KEY
+  apiKey: import.meta.env.API
 });
 
 export const getAIResponse = async (userMessage: string): Promise<string> => {
